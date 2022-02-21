c7d78731bdaf6c87054482cb8af382c990109e08
Sowa Takayanagi <55987516+tkyngs@users.noreply.github.com>
2022-02-21 13:21:21 +0900

    Add template

---
+++ ac/template.hs
@@ -0,0 +1,43 @@
+{-# OPTIONS_GHC -O2 #-}
+--{-# LANGUAGE StandaloneDeriving #-}
+--{-# LANGUAGE BangPatterns #-}
+--{-# LANGUAGE TupleSections #-}
+--{-# LANGUAGE Strict #-}
+ 
+module Main where
+ 
+import Debug.Trace
+--import Data.Bits
+--import Data.List
+--import Data.Char
+--import Data.Maybe
+--import Data.Function
+--import Data.Ratio
+--import Data.Bifunctor
+--import Data.Foldable
+--import Data.Tuple
+--import Data.Complex
+--import Data.STRef
+--import Data.Array.ST
+--import qualified Data.Array as A
+--import qualified Data.Array.Unboxed as AU
+--import qualified Data.Vector as V
+--import qualified Data.Vector.Mutable as VM
+--import qualified Data.Vector.Unboxed as VU
+--import qualified Data.Vector.Unboxed.Mutable as VUM
+--import qualified Data.Vector.Algorithms.Intro as VAI
+--import qualified Data.Vector.Algorithms.Search as VAS
+--import Control.Monad
+--import Control.Monad.ST
+--import qualified Data.ByteString.Char8 as BS
+--import qualified Data.Set as S
+import qualified Data.IntSet as IS
+--import qualified Data.Map.Strict as M
+--import qualified Data.IntMap.Strict as IM
+--import qualified Data.Sequence as SQ
+--import qualified Data.IntPSQ as IPSQ
+--import qualified Data.Heap as H
+
+main :: IO ()
+main=do
+
